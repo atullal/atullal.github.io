1e74ce68ccad2a2847527b34a1f1afd8405e0a23
Atul Lal <atul.lal123@gmail.com>
2024-04-03 00:24:27 +0530

    Removes mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-import withMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
