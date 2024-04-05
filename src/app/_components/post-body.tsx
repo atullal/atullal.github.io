cecc7be3dc2f67356599fbce5205d4a47e281294
Atul Lal <atul.lal123@gmail.com>
2024-04-05 23:08:47 +0530

    Fixes remark math

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,7 +1,5 @@
 import markdownStyles from "./markdown-styles.module.css";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import remarkMath from 'remark-math';
-
 type Props = {
   content: string;
 };
@@ -11,12 +9,7 @@ export function PostBody({ content }: Props) {
   return (
     <div className="mx-auto max-w-2xl">
       <div className={markdownStyles["markdown"]}>
-        <MDXRemote source={content} options={{
-          mdxOptions: {
-            remarkPlugins: [remarkMath],
-            format: 'mdx',
-          }
-        }}/>
+        <MDXRemote source={content} />
       </div>
     </div>
   );
