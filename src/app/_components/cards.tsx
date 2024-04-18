83dfc47e56ac2bf6fea8164ac00ad719534dcb26
Atul Lal <atul.lal123@gmail.com>
2024-04-19 03:37:03 +0530

    Fixes about section

---
+++ src/app/_components/cards.tsx
@@ -0,0 +1,76 @@
+import {FaLinkedinIn} from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+const Cards = () => {
+    return <section>
+        <div className="py-10">
+            <div className="mx-auto  max-w-6xl text-gray-500">
+                <div className="">
+                    <h2 className="text-3xl text-gray-950 dark:text-white font-semibold">Contact</h2>
+                    <p className="mt-6 text-gray-700 dark:text-gray-300">Check out my projects on GitHub or connect with me on LinkedIn if you're into tech, innovation, or just want to chat.</p>
+                </div>
+                <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                    <div className="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+                        <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-blue-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-5 dark:group-hover:opacity-10"></div>
+                        <div className="relative">
+                            <div className="text-white bg-[#0077b5] border border-blue-500/10 flex relative *:relative *:size-6 *:m-auto size-12 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[7px] text-white bg-[#0077b5]">
+                                
+                                <FaLinkedinIn xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"/>
+                            </div>
+    
+                            <div className="mt-6 pb-6 rounded-b-[--card-border-radius]">
+                                <p className="text-gray-700 dark:text-gray-300">Connect with me on LinkedIn.</p>
+                            </div>
+    
+                            <div className="flex -mb-8 py-8 border-t border-gray-200 dark:border-gray-800">
+    <a href="#" download="/" className="w-full rounded-xl disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-white bg-[#0077b5] hover:bg-[#005582] active:bg-[#004471] flex items-center text-sm h-8 px-3.5 justify-center">
+        <span>Connect</span>
+    </a>
+</div>
+
+
+                        </div>
+                    </div>
+                    <div className="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+                        <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-green-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-5 dark:group-hover:opacity-10"></div>
+                        <div className="relative">
+                            <div className="border border-green-500/10 flex relative *:relative *:size-6 *:m-auto size-12 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[7px]  bg-[#333] text-white">
+                            <svg className="size-5" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"></path></svg>
+                                </div>
+    
+                            <div className="mt-6 pb-6 rounded-b-[--card-border-radius]">
+                                <p className="text-gray-700 dark:text-gray-300">Check out my GitHub profile.</p>
+                            </div>
+    
+                            <div className="flex -mb-8 py-8 border-t border-gray-200 dark:border-gray-800">
+    <a href="#" download="/" className="w-full rounded-xl disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-white bg-[#333] hover:bg-[#24292e] active:bg-[#2c2f31] flex items-center text-sm h-8 px-3.5 justify-center">
+        <span>Check out</span>
+    </a>
+</div>
+                        </div>
+                    </div>
+                    <div className="relative group overflow-hidden p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+                        <div aria-hidden="true" className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-red-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-5 dark:group-hover:opacity-10"></div>
+                        <div className="relative">
+                            <div className="border border-red-500/10 flex relative *:relative *:size-6 *:m-auto size-12 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[7px] bg-[#D93025] text-white">
+              
+                            <MdEmail xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"/>
+                            </div>
+    
+                            <div className="mt-6 pb-6 rounded-b-[--card-border-radius]">
+                                <p className="text-gray-700 dark:text-gray-300">Drop a hey in my mailbox.</p>
+                            </div><div className="flex gap-3 -mb-8 py-8 border-t border-gray-200 dark:border-gray-800">
+    <a href="#" download="/" className="w-full rounded-xl disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-white bg-[#D93025] hover:bg-[#C5221F] active:bg-[#B31E1A] flex items-center text-sm h-8 px-3.5 justify-center">
+        <span>Mail</span>
+    </a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>;
+  };
+  
+export default Cards;
+  
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import Header from '../_components/header';
 import Container from '../_components/container';
 import Contribution from '../_components/contribution';
+import Cards from '../_components/cards';
+import Image from "next/image";
 const AboutPage: React.FC = () => {
   return (
 
@@ -15,28 +17,53 @@ const AboutPage: React.FC = () => {
             <meta name="description" content="Learn more about me and my interests." />
         </Head>
         <main>
-            <h1 className="text-3xl font-bold mb-4">About Me</h1>
-            <p className="text-lg text-gray-700 mb-8">
-            Welcome to my personal website! Here's a little bit about me...
-            </p>
+          <div className="flex flex-col md:flex-row">
+  {/* Image container */}
+  <div className="w-full md:w-1/3 p-8 md:pl-0 flex justify-center items-center md:items-start">
+    <Image
+      src={'/assets/blog/authors/atullal_large.png'}
+      alt={`Image of Atul Lal`}
+      className="w-full md:max-w-xs h-auto object-cover"
+      width={120}
+      height={120}
+    />
+  </div>
+
+  {/* Text container */}
+  <div className="w-full md:w-2/3 pt-8">
+    <h1 className="text-3xl font-bold mb-4">About Me</h1>
+    <p className="text-lg text-gray-700 mb-8">
+      Welcome to my site! I'm Atul Lal, a Software Engineer specializing in distributed systems and cloud computing, and a Boston University alum. At Commvault Systems, I fine-tuned APIs and scaled microservices—call me a code whisperer.
+    </p>
+  </div>
+</div>
+
+
+
+
             <section className="mb-8">
             <h2 className="text-xl font-semibold mb-4">Background</h2>
             <p className="text-lg text-gray-700">
-                I'm a passionate developer who loves to create innovative solutions using cutting-edge technologies.
+            My adventure in technology started at Bennett University, where my love for solving complex problems was ignited. After earning my bachelor’s degree, I took the plunge into the professional world as a Software Engineer at Commvault Systems. For over two years, I was at the frontline of innovation, tackling challenges that pushed the boundaries of what our technology could achieve.<br/><br/>
+
+Driven by a desire to delve deeper, I later pursued a Master's degree at Boston University. Here, I not only expanded my technical expertise but also embraced opportunities to lead and innovate, further shaping my approach to software development and system optimization. My tools? A whole lot of Go, Python, and a dash of C++.<br/><br/>
+
+But there’s more to me than code and cloud solutions. Once the computers power down, my artistic side takes the stage. I’m a self-proclaimed jack of all trades, dabbling in everything from perfecting my cup of coffee to painting my sleepless nights’ wildest dreams. My passion for art extends to woodworking and painting denim jackets, each project a new expression of my creative spirit.<br/><br/>
+
+I'm also a die-hard Real Madrid fan and a movie connoisseur—and critic! Each of these passions enriches my life, providing balance and inspiration in everything I do.<br/><br/>
+
+Want to talk tech, art, or the latest blockbuster?
             </p>
-            </section>
-            <section className="mb-8">
-            <h2 className="text-xl font-semibold mb-4">Interests</h2>
-            <p className="text-lg text-gray-700">
-                My interests include web development, artificial intelligence, and gaming.
-            </p>
-            </section>
-            <section>
-            <h2 className="text-xl font-semibold mb-4">Contact</h2>
-            <p className="text-lg text-gray-700">You can reach out to me at example@example.com</p>
-            </section>
+            </section><br/>
         </main>
+
+        <section>
+            <h2 className="text-xl font-semibold mb-4">Contribution</h2>
+            <p className="text-lg text-gray-700">Tossing my GitHub contribution graph up here to gently shame myself into coding more! 🌟💻 </p><br/>
         <Contribution />
+            </section>
+
+        <Cards />
         </div>
       </Container>
     </main>
